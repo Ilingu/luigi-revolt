0142777b6c3795e1725467bac68bfd7b869b03e1
Ilingu <ilingu@duck.com>
2022-12-02 13:14:29 +0100

    shutdown autocleaner (to lower the server expenses, because AC was never used)

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import "dotenv/config";
-import StartACBot from "./Services/AutoCleaner/bot";
 import StartCronReceiverServer from "./Services/server/receiver";
 import { RegisterAllService } from "./Services/server/register";
 import StartLuigiBot from "./Services/Luigi/bot";
+// import StartACBot from "./Services/AutoCleaner/bot";
 
 // Where the app start
 const mainProcess = async () => {
   // Bots
   StartLuigiBot();
-  StartACBot();
+  // StartACBot(); --> shutted down because useless
 
   // Server
   await StartCronReceiverServer();
